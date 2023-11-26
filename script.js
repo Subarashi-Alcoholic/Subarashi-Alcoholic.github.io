7236b8c2f76633ab6b31729e9e0057b216038eb0
Посметюк Антон <112932091+Subarashi-Alcoholic@users.noreply.github.com>
2023-11-26 21:11:47 +0700

    update

--- script.js
+++ script.js
@@ -219,7 +219,23 @@ window.onload = function(){
         }
     };
 
-    changeColorButton = document.getElementById("btn_swap_color"); //смена цвета окна
+    changeColorButton = document.getElementById("btn_swap_bgcolor"); //смена цвета фона
+    changeColorButton.onclick = function() {
+        const randomColor = getRandomColor();
+        document.body.style.backgroundColor = randomColor;
+};
+
+    // Вспомогательная функция для генерации случайного цвета
+    function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    changeColorButton = document.getElementById("btn_swap_rescolor"); //смена цвета окна
     changeColorButton.onclick = function() {
         const randomColor = getRandomColor();
         outputElement.style.backgroundColor = randomColor;
