0d8a06685d63e21f9bb01d42a893858e40531970
Посметюк Антон <112932091+Subarashi-Alcoholic@users.noreply.github.com>
2024-01-04 14:35:41 +0700

    update

--- jsHUB.js
+++ jsHUB.js
@@ -1,7 +1,20 @@
 function blinkCard(card) {
-    card.classList.add("blink");
-    setTimeout(function() {
-      card.classList.remove("blink");
-    }, 1000);
-  }
-  
+  card.classList.add("blink");
+  setTimeout(function() {
+    card.classList.remove("blink");
+  }, 1000);
+}
+
+function changeColor() {
+  var cardS = document.querySelector(".cardS");
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  var rgbColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+  cardS.style.transition = "background-color 0.5s"; // Add transition property
+  cardS.style.backgroundColor = rgbColor;
+}
+
+setInterval(changeColor, 250);
+
+
